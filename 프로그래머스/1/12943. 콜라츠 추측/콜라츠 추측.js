a82dbab5ac91f5f697d48ac266af0d35a7c5461f
Jaidy <hyjgood2000@naver.com>
2025-07-03 15:57:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    let count = 0;
+    
+    while(num !== 1 && count < 500 ) {
+        if(num % 2 === 0){
+            num = num / 2;
+            count++;
+        } else {
+            num = num * 3 + 1;
+            count++;
+        }
+    }
+    
+    return count === 500 ? -1 : count ;
+}
