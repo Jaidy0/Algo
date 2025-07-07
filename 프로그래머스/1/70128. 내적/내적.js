067dc3abbf84e4df23212d1d1a3e0e15098d06c5
Jaidy <hyjgood2000@naver.com>
2025-07-07 23:12:01 +0900

    [level 1] Title: 내적, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    // let arrLength = a.length;
+    let sum = 0;
+    // for(let i = 0; i < arrLength; i++) {
+    //     sum += a[i] * b[i];
+    // }
+    
+    return a.reduce((sum, curr, idx) => sum += curr * b[idx], 0);
+    
+}
