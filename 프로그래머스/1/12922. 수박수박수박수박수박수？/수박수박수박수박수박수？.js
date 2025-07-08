ca405b6eead20c212bffdb158bf18ae8faaee0a4
Jaidy <hyjgood2000@naver.com>
2025-07-08 09:49:31 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,13 +1,14 @@
 function solution(n) {
     
-    let ans = "";
-    for(let i = 0; i < n; i++) {
-        if(i % 2 === 0) {
-            ans += "수";   
-        } else { 
-            ans += "박";
-        }
-    }
+    // let ans = "";
+    // for(let i = 0; i < n; i++) {
+    //     if(i % 2 === 0) {
+    //         ans += "수";   
+    //     } else { 
+    //         ans += "박";
+    //     }
+    // }
     
-    return ans;
+    return "수박".repeat(Math.ceil(n/2)).substring(0,n);
+    // return ans;
 }
