9d3f0810e4b37b56656e4d60a9045f60d0a47e7f
Jaidy <hyjgood2000@naver.com>
2025-08-04 17:03:44 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+#  Python(256)이나 C# 스킬(1024)을 가진 개발자의 정보(ID, 이메일, 이름, 성을 조회)를 조회
+SELECT DISTINCT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS d, SKILLCODES s 
+# WHERE (SKILL_CODE & 256 = 256) OR (SKILL_CODE & 1024 = 1024) 
+WHERE s.NAME IN ('Python', 'C#') 
+  AND (d.SKILL_CODE & s.CODE = s.CODE)
+ORDER BY ID;
