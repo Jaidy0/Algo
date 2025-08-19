140c67befd142fbbca651a475b41a980d2cdc26f
Jaidy <hyjgood2000@naver.com>
2025-08-19 13:06:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 107 ms, Memory: 28,160 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -4,9 +4,7 @@ public class Solution {
     static int N,M,R,C,L;
     static int[][] map;
     static boolean[][] V;
-//    static int[] dr = {0,0,-1,1};
-//    static int[] dc = {-1,1,0,0};
-    // 상{-1,0} 하0{1,} 좌{0,-1} 우{0,1}
+
     static int[][][] move =
             {{{-1,0,0},{1,0,1},{0,-1,2},{0,1,3}} // 상하좌우 0123
             ,{{-1,0,0},{1,0,1}} // 상하
@@ -19,35 +17,7 @@ public class Solution {
 
 
     public static void main(String[] args) throws IOException {
-        /*
-            탈주범은 맨홀 뚜껑으로 유입
-            터널끼리 연결되 있는 경우 이동 가능 -> 탈주범이 있을 수 있는 위치 계산
-            시간 당 1의 거리 이동
-            지하터널은 총 7종류의 터널 구조물로 구성됨
-            1 : 상하좌우
-            2 : 상하
-            3 : 좌우
-            4 : 상우
-            5 : 하우
-            6 : 하좌
-            7 : 상좌
 
-            이동 방향 : 다음 뚫린 방향
-            상 : 하
-            하 : 상
-            좌 : 우
-            우 : 좌
-
-            맨홀 뚜껑 위치 세로위치 r, 가로위치 c
-
-            [ 아이디어 ]
-            - bfs로 시간만큼 이동하면서 탈주범이 이동할 수 있는 경로 방문 체크
-            - 경우에 따른 이동 경로는 switch 문으로 설정한다.
-            - 상하좌우더라도 그쪽으로 무조건 이동할 수 있는게 아니다.
-            - 이동 방향으로 다음 위치가 뚤려있는지 확인해야 한다.
-
-
-         */
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 세로 크기 N, 가로 크기 M, 멘홀뚜껑 R,C / 탈출 후 소요 시간 L
@@ -72,14 +42,12 @@ public class Solution {
                 }
             }
 
-//            System.out.println(Arrays.deepToString(map));
 
             V = new boolean[N][M];
 
             bfs();
 
             int ans = 0;
-//            System.out.println(Arrays.deepToString(V));
             for(int r = 0; r < N; r++) {
                 for(int c = 0; c < M; c++) {
                     if(V[r][c]) ans++;
@@ -104,7 +72,6 @@ public class Solution {
             int l = tmp[2];
 
             if(l >= L) {
-//                System.out.println("시간이 지났습니다 ::: " + r + "/" + c + "/" + l);
                 continue;
             }
 
@@ -114,7 +81,6 @@ public class Solution {
             int nr,nc;
 
             for(int i = 0; i < movArr.length; i++) {
-//                System.out.println("movArr" + map[r][c] + Arrays.toString(movArr[i]));
                 nr = r + movArr[i][0];
                 nc = c + movArr[i][1];
 
